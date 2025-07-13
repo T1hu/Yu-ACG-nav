d1b9b796232100ab54cd122940c90fd25eeb369c
Star Dew on the Other Shore <zyen777@163.com>
2025-07-14 03:16:43 +0800

    Update KunAbout.tsx

--- src/components/KunAbout.tsx
+++ src/components/KunAbout.tsx
@@ -8,43 +8,35 @@ export const KunAbout = () => {
 
   return (
     <div class="space-y-1">
+      {/* 英文说明 */}
       <Show when={context.locale === 'en-us'}>
         <p>
-          This website is completely open-source! The source code is available
-          on{' '}
+          This site is a modified version based on the KUN Visual Novel Navigation Page, and its source code is
+          available on{' '}
           <a
             class="underline underline-offset-2 decoration-kun-blue5-light dark:decoration-kun-blue5-dark"
-            href="https://github.com/KUN1007/kun-galgame-nav-solid"
+            href="https://github.com/T1hu/Yu-ACG-nav"
             target="_blank"
             rel="noopener noreferrer"
           >
             GitHub
           </a>
-          . We built this site using the latest{' '}
-          <a
-            class="underline underline-offset-2 decoration-kun-blue5-light dark:decoration-kun-blue5-dark"
-            href="https://start.solidjs.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            SolidStart
-          </a>{' '}
-          framework
+          . We appreciate the original open-source project.
         </p>
         <p>
-          We recommend you press `Ctrl + D` to bookmark this page to avoid
-          losing contact with us.
+          We recommend you press <code>Ctrl + D</code> to bookmark this page to avoid losing contact with us.
         </p>
         <p>
           You can join our{' '}
           <a
             class="underline underline-offset-2 decoration-kun-blue5-light dark:decoration-kun-blue5-dark"
-            href="https://t.me/kungalgame"
+            href="https://t.me/xiaoyuO721"
             target="_blank"
             rel="noopener noreferrer"
           >
             Telegram group
           </a>
+          .
         </p>
         <p>
           <A
@@ -54,46 +46,35 @@ export const KunAbout = () => {
             {t('kun.loli')}
           </A>
         </p>
-        <br />
-        <p>
-          Thank you to every organization and friend contributing to the
-          open-source community!
-        </p>
+        <p>Thank you to every organization and friend contributing to the open-source community!</p>
       </Show>
 
+      {/* 简体中文说明 */}
       <Show when={context.locale === 'zh-cn'}>
         <p>
-          本网站完全开源! 源码在{' '}
+          本站基于 KUN Visual Novel Navigation Page 二次编写生产，源码在{' '}
           <a
             class="underline underline-offset-2 decoration-kun-blue5-light dark:decoration-kun-blue5-dark"
-            href="https://github.com/KUN1007/kun-galgame-nav-solid"
+            href="https://github.com/T1hu/Yu-ACG-nav"
             target="_blank"
             rel="noopener noreferrer"
           >
             GitHub
           </a>
-          。 我们使用最现代的{' '}
-          <a
-            class="underline underline-offset-2 decoration-kun-blue5-light dark:decoration-kun-blue5-dark"
-            href="https://start.solidjs.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            SolidStart
-          </a>{' '}
-          框架编写该网站
+          ，在这里十分感谢这份开源项目。
         </p>
-        <p>我们建议您 `Ctrl + D` 收藏该网页以防止与我们失去联系</p>
+        <p>我们建议您 <code>Ctrl + D</code> 收藏该网页以防止与我们失去联系。</p>
         <p>
           您可以加入我们的{' '}
           <a
             class="underline underline-offset-2 decoration-kun-blue5-light dark:decoration-kun-blue5-dark"
-            href="https://t.me/kungalgame"
+            href="https://t.me/xiaoyuO721"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Telegram 群组
+            Telegram 交流群
           </a>
+          。
         </p>
         <p>
           <A
@@ -103,7 +84,6 @@ export const KunAbout = () => {
             {t('kun.loli')}
           </A>
         </p>
-        <br />
         <p>感谢为开源事业做出贡献的每个组织和朋友！</p>
       </Show>
     </div>
