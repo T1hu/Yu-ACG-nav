68ec294e52b07b7f8a813d703159f75e999f2e37
Star Dew on the Other Shore <zyen777@163.com>
2025-07-15 01:58:18 +0800

    Update entry-server.tsx

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -31,32 +31,32 @@ export default createHandler(() => (
           <meta name="theme-color" content="#0969da" />
           <meta name="msapplication-TileColor" content="#0969da" />
           <meta name="theme-color" content="#0969da" />
-          <meta name="og:title" content="KUN Visual Novel | 鲲 Galgame" />
-          <meta name="og:url" content="https://www.kungal.com" />
+          <meta name="og:title" content="The Star of the Other Shore | 彼岸星露-羽ACG" />
+          <meta name="og:url" content="https://seve.yugal.cc" />
           <meta name="og:type" content="article" />
           <meta name="og:image:width" content="1920" />
           <meta name="og:image:height" content="1080" />
           <meta
             name="og:image:url"
-            content="https://www.kungal.com/kungalgame.webp"
+            content="https://img.yugal.cc/i/2025/07/15/2kjkfo.webp"
           />
           <meta
             name="og:image:secure_url"
-            content="https://www.kungal.com/kungalgame.webp"
+            content="https://img.yugal.cc/i/2025/07/15/2kjkfo.webp"
           />
           <meta name="twitter:card" content="summary_large_image" />
           <meta
             name="twitter:image"
-            content="https://www.kungal.com/kungalgame.webp"
+            content="https://img.yugal.cc/i/2025/07/15/2kjkfo.webp"
           />
           <meta name="twitter:site" content="@kungalgame" />
           <meta
             name="og:description"
-            content="KUN Visual Novel, Visual Novel Forum | 鲲 Galgame, 论坛, Galgame 资源, Galgame 社区, Galgame 技术, Galgame 交流"
+            content="The Star of the Other Shore | 彼岸星露-羽ACG | 专注分享免费Galgame，涵盖PC、安卓、模拟器，希望在这里能找到你喜欢的游戏~"
           />
           <meta
             name="description"
-            content="KUN Visual Novel Navigation Page | 鲲 Galgame 导航页, 开源免费, Galgame 论坛, Galgame 资源, Galgame 社区"
+            content="The Star of the Other Shore | 彼岸星露-羽ACG | 专注分享免费Galgame，涵盖PC、安卓、模拟器，希望在这里能找到你喜欢的游戏~"
           />
           <link rel="icon" type="image/webp" href="/favicon.webp" />
           {assets}
